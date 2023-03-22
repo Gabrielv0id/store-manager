80800bb1ba07e54089606a5a0e2c02f45277acde
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-03-22 09:27:26 -0300

    feat 1: meu servidor nao abre

---
+++ src/models/products.model.js
@@ -0,0 +1,22 @@
+const connection = require('./db/connection');
+
+const findAll = async () => {
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products',
+  );
+
+  return result;
+};
+
+const findById = async (productId) => {
+  const [[product]] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?',
+    [productId],
+  );
+  return product;
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
---
+++ src/services/products.service.js
@@ -0,0 +1,19 @@
+const { productsModel } = require('../models');
+
+const findAll = async () => {
+  const products = await productsModel.findAll();
+  return { type: null, message: products };
+};
+
+const findById = async (id) => {
+  const product = await productsModel.findById(id);
+
+  if (!product) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+
+  return { type: null, message: product };
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
